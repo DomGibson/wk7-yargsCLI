d7386b257c01b15912f2c37031f5cbcec276678b
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-06-14 12:47:28 +0100

    Add files via upload

---
+++ my.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE users (
+--     id int,
+--     first_name varchar(255),
+--     last_name varchar(255),
+--     fav_film varchar(255),
+--     fav_number int,
+-- );
+
+-- SELECT * FROM table_name;
+
+-- ALTER TABLE table_name
+-- ADD column_name datatype;
+
+-- DELETE FROM table[WHERE conditions];
+-- DELETE FROM table WHERE field_name = 'thing';
